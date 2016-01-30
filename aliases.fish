c9281cf82958a3b131ae25641f752c6c5882433a
Johan Larsson <johan@johanl.se>
2016-01-30 19:26:35 +0100

    Add stream suit atom alias

--- aliases.fish
+++ aliases.fish
@@ -1,3 +1,4 @@
 alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder'
 alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder'
 
+alias streamsuit='atom $GOPATH/src/github.com/streamsuit/'
