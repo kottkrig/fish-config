894f5e6838ee80fa057bd00152c83fc127461782
Johan Larsson <johan.larsson@etc.se>
2019-07-08 09:11:22 +0200

    Alias ls to ls -l1

--- aliases.fish
+++ aliases.fish
@@ -1,2 +1,4 @@
 alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder'
 alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder'
+
+alias ls 'ls -l1'
