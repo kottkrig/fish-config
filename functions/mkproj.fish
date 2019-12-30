be11aa2d01a2d3eddff1b9fe4a8a65e9c4595e79
Johan Larsson <johan.larsson@etc.se>
2019-12-30 15:10:47 +0100

    Create subdirectories only if main directory was created successfully

--- functions/mkproj.fish
+++ functions/mkproj.fish
@@ -1,11 +1,11 @@
 function mkproj -d "Create a project directory and set CWD"
     command mkdir $argv
-    command mkdir "$argv/Admin" "$argv/Input" "$argv/Output" "$argv/WIP"
     if test $status = 0
         switch $argv[(count $argv)]
             case '-*'
 
             case '*'
+                command mkdir "$argv/Admin" "$argv/Input" "$argv/Output" "$argv/WIP"
                 cd $argv[(count $argv)]
                 return
         end
